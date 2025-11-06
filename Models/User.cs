f7be3b7df562b15f333aac3db82ab0ac087bff22
hienvm <hienvmhe163953@fpt.edu.vn>
2025-11-06 15:34:21 +0700

    upadte model

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string? Role { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public virtual ICollection<Contact> Contacts { get; set; } = new List<Contact>();
+}
